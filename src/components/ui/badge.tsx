f6b84a9558d298c1c1265b65d7ec7911c0e4aa01
AhmedAtta <ahm.adu.atta66@gmail.com>
2025-11-26 16:46:57 +0200

    Update badge.tsx

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,4 +1,4 @@
-import { Badge } from "./components/ui/Badge";
+import { Badge } from "@/components/ui/badge";
 import * as React from "react";
 import { cva, type VariantProps } from "class-variance-authority";
 
